457c21ac2600d7e3b4c69ed0a9dc1b4739fc5678
Nicholas Babb <nbabb@ideally.com>
2018-04-10 14:06:09 -0400

    Properly reference instance variable, otherwise defined? always returns nil)

--- lib/rspec-webservice-outputter.rb
+++ lib/rspec-webservice-outputter.rb
@@ -3,7 +3,7 @@ require 'rspec-webservice-outputter/base'
 
 RSpec.configure do |config|
   config.after(:each) do
-    next unless ENV['WEBSERVICE'] && defined?(request)
+    next unless ENV['WEBSERVICE'] && defined?(@request)
     RspecWebServiceOutputter::Output.new(request, response).print
   end
 end
